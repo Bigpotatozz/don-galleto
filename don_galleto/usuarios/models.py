d1dffa6c455793b5828ca2e1373ce746b6d31eba
oscar <oscarmancilla227@gmail.com>
2025-03-12 13:44:25 -0600

    modelos agregados

--- don_galleto/usuarios/models.py
+++ don_galleto/usuarios/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Usuario(models.Model):
+    id_usuario = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    telefono = models.CharField(max_length=10)
+    correo = models.EmailField(max_length=60)
+    contrasenia = models.CharField(max_length=45)
+    rol = models.CharField(max_length=10, choices=[('empleado', 'empleado'), ('cliente', 'cliente')])
+    codigo_verificacion = models.IntegerField()
+    
+    def __str__(self):
+        return f" {self.nombre} - {self.telefono} - {self.correo} - {self.contrasenia} - {self.rol} - {self.codigo_verificacion}"
--- don_galleto/ventas/models.py
+++ don_galleto/ventas/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from usuarios.models import Usuario
+from galletas.models import Galleta
 # Create your models here.
+class Venta(models.Model):
+    id_venta = models.AutoField(primary_key=True)
+    fecha_venta = models.DateField()
+    total = models.FloatField()
+    tipo = models.CharField(max_length=30)
+    estatus = models.CharField(max_length=30)
+    id_usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE, null=False, related_name='venta_cliente')
+    
+    def __str__(self):
+        return f" {self.fecha_venta} - {self.total} - {self.tipo} - {self.estatus} - {self.id_usuario}"
+    
+    
+class Detalle_venta(models.Model):
+    id_detalle_venta = models.AutoField(primary_key=True)
+    cantidad = models.FloatField()
+    id_venta = models.ForeignKey(Venta, on_delete=models.CASCADE, null=False, related_name='detalle_venta_venta')
+    id_galleta = models.ForeignKey(Galleta, on_delete=models.CASCADE, null=False, related_name='detalle_venta_galleta')
+    
+    def __str__(self):
+        return f" {self.cantidad} - {self.id_venta} - {self.id_galleta}"
