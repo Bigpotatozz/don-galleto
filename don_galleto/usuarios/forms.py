8a2e65c5e2a94bdea7fa3337aed0a26ecbb0bb23
Oscar Mancilla <oscarmancilla227@gmail.com>
2025-03-18 19:30:07 -0600

    cru usuarios

--- don_galleto/usuarios/forms.py
+++ don_galleto/usuarios/forms.py
@@ -1,5 +1,51 @@
 from django import forms
 from usuarios.models import Usuario
+from django.contrib.auth.forms import UserCreationForm
 
-
+class Registro_admin_form(UserCreationForm):
         
+        telefono = forms.CharField(max_length=10)
+        rol = forms.ChoiceField(choices=[('empleado', 'Empleado'), ('cliente', 'Cliente')])
+        
+        class Meta: 
+            model = Usuario
+            fields = ['username', 'email', 'password1', 'password2', 'telefono', 'rol']
+
+class Edicion_usuario_form(forms.ModelForm):
+      
+    username = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    contrasenia = forms.CharField(max_length=100, widget=forms.PasswordInput, required=False)
+    telefono = forms.CharField(max_length=10)
+    rol = forms.ChoiceField(choices=[('empleado', 'Empleado'), ('cliente', 'Cliente')])
+
+
+    class Meta:
+          model = Usuario
+          fields = ['username', 'email', 'contrasenia', 'telefono', 'rol']
+
+    def save(self, id):
+        
+        usuario = Usuario.objects.get(id_usuario=id)
+        usuario.username = self.cleaned_data['username']
+        usuario.email = self.cleaned_data['email']
+        usuario.telefono = self.cleaned_data['telefono']
+        usuario.rol = self.cleaned_data['rol']
+        nueva_contrasena = self.cleaned_data.get('contrasenia')
+
+        if nueva_contrasena:
+             usuario.set_password(nueva_contrasena)
+        
+        usuario.save()
+
+class Eliminar_usuario_form(forms.ModelForm):
+
+    class Meta:
+        model = Usuario
+        fields = ['is_active']
+
+    def save(self , id):
+        usuario = Usuario.objects.get(id_usuario=id)
+        usuario.is_active = False
+        usuario.save()
+    
--- don_galleto/usuarios/views.py
+++ don_galleto/usuarios/views.py
@@ -19,3 +19,46 @@ class Lista_usuarios_view(TemplateView):
             "usuarios": usuarios
         }
         
+
+class Registro_admin_view(FormView):
+    template_name = 'agregar_usuario.html'
+    form_class = forms.Registro_admin_form
+    success_url = reverse_lazy('listado_usuarios')
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+    
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+    
+class Edicion_usuario_view(FormView):
+    template_name = 'editar_usuario.html'
+    form_class = forms.Edicion_usuario_form
+    success_url = reverse_lazy('listado_usuarios')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        id = self.kwargs.get('id')
+        usuario = get_object_or_404(Usuario, id_usuario=id)
+        kwargs['instance'] = usuario
+        return kwargs
+
+    def form_valid(self, form):
+        form.save(self.kwargs.get('id'))
+        return super().form_valid(form)
+
+class Eliminar_usuario_view(FormView):
+    form_class = forms.Eliminar_usuario_form
+    success_url = reverse_lazy('listado_usuarios')
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        id = self.kwargs.get('id')
+        usuario = get_object_or_404(Usuario, id_usuario=id)
+        kwargs['instance'] = usuario
+        return kwargs
+
+    def form_valid(self, form):
+        form.save(self.kwargs.get('id'))
+        return super().form_valid(form)
