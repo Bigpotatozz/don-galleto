62605afb9a7b96ae87d91ce06a424886c16693ba
LaAlejandraDev <alejandrasauceda2200@gmail.com>
2025-03-19 14:40:18 -0600

    Configuración de settings en mi rama

--- don_galleto/don_galleto/settings.py
+++ don_galleto/don_galleto/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pfdg',
         'USER': 'root',
-        'PASSWORD': '1924',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '3306',
     }
