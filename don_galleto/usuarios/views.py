c443f3989653c48e6dd2747bbe9b5fd4ddf91b3e
oscar <oscarmancilla227@gmail.com>
2025-03-19 23:17:37 -0600

    views de usuarios protegidas

--- don_galleto/usuarios/views.py
+++ don_galleto/usuarios/views.py
@@ -11,13 +11,13 @@ from usuarios.utils import asignar_permisos
 
 
 
-class Lista_usuarios_view(TemplateView, PermissionRequiredMixin):
+class Lista_usuarios_view(PermissionRequiredMixin, TemplateView):
     
     permission_required = 'usuarios.admin'
-
+    template_name = 'lista_usuarios.html'
     def handle_no_permission(self):
         return redirect('home')
-    template_name = 'lista_usuarios.html'
+    
     
     def get_context_data(self):
         usuarios = Usuario.objects.all()
@@ -27,10 +27,17 @@ class Lista_usuarios_view(TemplateView, PermissionRequiredMixin):
         }
         
 
-class Registro_admin_view(FormView):
+class Registro_admin_view(PermissionRequiredMixin,FormView):
+  
+    permission_required = 'usuarios.admin'
+
+    def handle_no_permission(self):
+        return redirect('home')
+    
     template_name = 'agregar_usuario.html'
     form_class = forms.Registro_admin_form
     success_url = reverse_lazy('listado_usuarios')
+    
     
     def form_valid(self, form):
         
@@ -41,6 +48,12 @@ class Registro_admin_view(FormView):
         return super().form_invalid(form)
     
 class Edicion_usuario_view(FormView):
+    
+    permission_required = 'usuarios.admin'
+
+    def handle_no_permission(self):
+        return redirect('home')
+    
     template_name = 'editar_usuario.html'
     form_class = forms.Edicion_usuario_form
     success_url = reverse_lazy('listado_usuarios')
@@ -59,6 +72,7 @@ class Edicion_usuario_view(FormView):
 
 
 def eliminar_usuario(request, id):
+        
     usuario = get_object_or_404(Usuario, id_usuario=id)
     usuario.is_active = False
     usuario.save()
