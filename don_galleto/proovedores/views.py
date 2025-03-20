36a75f0d48744edb46f926d44b837551f6551770
oscar <oscarmancilla227@gmail.com>
2025-03-20 14:26:45 -0600

    modulo de proovedores integrado

--- don_galleto/proovedores/views.py
+++ don_galleto/proovedores/views.py
@@ -4,9 +4,18 @@ from django.views.generic import FormView
 from proovedores.models import Proovedor
 from . import forms
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.shortcuts import redirect
 
 # Create your views here.
-class CrearProovedorView(FormView):
+class CrearProovedorView(PermissionRequiredMixin,FormView):
+    
+    
+    permission_required = 'usuarios.empleado'
+    def handle_no_permission(self):
+        return redirect('home')
+    
+    
     template_name = "crear_proovedor.html"  
     form_class = forms.ProovedorRegistrarForm
     success_url = reverse_lazy("lista_proovedor")
@@ -15,12 +24,23 @@ class CrearProovedorView(FormView):
         return super().form_valid(form)
 
 class ListaProovedorView(TemplateView):
+    
+    permission_required = 'usuarios.empleado'
+    def handle_no_permission(self):
+        return redirect('home')
+    
+    
     template_name = "lista_proovedores.html"
     def get_context_data(self):
         lista = Proovedor.objects.all()
         return {"lista": lista}
 
 class EditarProovedorView(FormView):
+    
+    permission_required = 'usuarios.empleado'
+    def handle_no_permission(self):
+        return redirect('home')
+    
     template_name = "editar_proovedor.html"
     form_class = forms.ProovedorEditarForm
     success_url = reverse_lazy("lista_proovedor")
