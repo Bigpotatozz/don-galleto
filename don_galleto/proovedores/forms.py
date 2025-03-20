e0a77cfed44a9cfebdcbfa03d1ecb461e176635d
EdiYt <erickvillaseor@yahoo.com.mx>
2025-03-19 23:33:03 -0600

    Ya se puede editar un proovedor

--- don_galleto/proovedores/forms.py
+++ don_galleto/proovedores/forms.py
@@ -29,3 +29,30 @@ class ProovedorRegistrarForm(forms.Form):
             estatus = self.cleaned_data['estatus']
         )
         proovedor.save()
+
+
+class ProovedorEditarForm(forms.ModelForm):
+    nombre = forms.CharField(max_length=30, label='Nombre')
+    nombre_contacto = forms.CharField(max_length=30, label='Nombre de contacto')
+    telefono = forms.CharField(max_length=12, label='Telefono')
+    correo = forms.EmailField(max_length=45, label='Correo')
+    direccion = forms.CharField(max_length=100, label='Direccion')
+    ciudad = forms.CharField(max_length=15, label='Ciudad')
+    estado = forms.CharField(max_length=15, label='Estado')
+    codigo_postal = forms.CharField(max_length=7, label='Codigo postal')
+    estatus = forms.ChoiceField(choices=[('activo', 'Activo'), ('inactivo', 'Inactivo'), ('bloqueado', 'Bloqueado')])
+    class Meta:
+        model = models.Proovedor
+        fields = ['nombre', 'nombre_contacto', 'telefono', 'correo', 'direccion', 'ciudad', 'estado', 'codigo_postal', 'estatus']
+    def save(self):
+        proovedor = self.instance
+        proovedor.nombre = self.cleaned_data['nombre']
+        proovedor.nombre_contacto = self.cleaned_data['nombre_contacto']
+        proovedor.telefono = self.cleaned_data['telefono']
+        proovedor.correo = self.cleaned_data['correo']
+        proovedor.direccion = self.cleaned_data['direccion']
+        proovedor.ciudad = self.cleaned_data['ciudad']
+        proovedor.estado = self.cleaned_data['estado']
+        proovedor.codigo_postal = self.cleaned_data['codigo_postal']
+        proovedor.estatus = self.cleaned_data['estatus']
+        proovedor.save()
--- don_galleto/proovedores/views.py
+++ don_galleto/proovedores/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.base import TemplateView
 from django.views.generic import FormView
 from proovedores.models import Proovedor
@@ -19,3 +19,20 @@ class ListaProovedorView(TemplateView):
     def get_context_data(self):
         lista = Proovedor.objects.all()
         return {"lista": lista}
+
+class EditarProovedorView(FormView):
+    template_name = "editar_proovedor.html"
+    form_class = forms.ProovedorEditarForm
+    success_url = reverse_lazy("lista_proovedor")
+    
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        id = self.kwargs.get("id")
+        proovedor = get_object_or_404(Proovedor, id_proovedor=id)
+        kwargs['instance'] = proovedor
+        return kwargs
+
+    
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
