22d66a5f172caacc4c906a70054087db299a45e8
oscar <oscarmancilla227@gmail.com>
2025-03-24 10:30:59 -0600

    forms insumos modificados

--- don_galleto/inventario_insumos/forms.py
+++ don_galleto/inventario_insumos/forms.py
@@ -6,7 +6,7 @@ from inventario_insumos.models import Compra_insumo
 class Registro_insumo_form(forms.ModelForm):
     nombre = forms.CharField(max_length=45)
     tipo = forms.ChoiceField(choices=[('base', 'Base'), ('complemento', 'Complemento')])
-    tipo_medida = forms.ChoiceField(choices=[('gramos', 'Gramos'), ('mililitros', 'Mililitros')])
+    tipo_medida = forms.ChoiceField(choices=[('g', 'gramos'), ('ml', 'mililitros')])
     
     class Meta:
         model = Insumo
@@ -24,12 +24,12 @@ class Registro_compra_insumo_form(forms.ModelForm):
     nombre = forms.CharField(max_length=45)
     cantidad = forms.FloatField()
     caducidad = forms.DateField(widget=forms.DateInput(attrs={'type': 'date'}))
-    precio = forms.FloatField()
+    
 
     id_proovedor_id = forms.ModelChoiceField(
         queryset = Proovedor.objects.all(),
         label = "proovedor",
-        to_field_name = "id_proovedor" 
+        to_field_name = "id_proovedor"
     )
 
     id_insumo_id = forms.ModelChoiceField(
@@ -40,27 +40,32 @@ class Registro_compra_insumo_form(forms.ModelForm):
 
     class Meta:
         model = Compra_insumo
-        fields = ['nombre', 'cantidad','caducidad', 'precio', 'id_proovedor_id', 'id_insumo_id']
+        fields = ['nombre', 'cantidad','caducidad', 'total','id_proovedor_id', 'id_insumo_id']
 
     def save(self, commit = True):
         nombre = self.cleaned_data['nombre']
         cantidad = self.cleaned_data['cantidad']
         caducidad = self.cleaned_data['caducidad']
-        precio = self.cleaned_data['precio']
+        total = self.cleaned_data['total']
         id_proovedor_id = self.cleaned_data['id_proovedor_id'].id_proovedor
         id_insumo_id = self.cleaned_data['id_insumo_id'].id_insumo
+        precio_unitario = total / cantidad
+        
+        
         new_compra_insumo = Compra_insumo(nombre = nombre, 
                                           cantidad = cantidad, 
-                                          cantidad_restante = 0,
+                                          cantidad_restante = cantidad,
                                           caducidad = caducidad, 
-                                          precio = precio, 
+                                          total = total,
+                                          precio_unitario = precio_unitario,
+                                          estatus = "disponible",
                                           id_proovedor_id = id_proovedor_id, 
                                           id_insumo_id = id_insumo_id)
 
-        
         insumo = Insumo.objects.get(id_insumo = id_insumo_id)
         insumo.cantidad = insumo.cantidad + cantidad
         insumo.estatus = "disponible"
+        insumo.precio_unitario = precio_unitario
 
         
 
