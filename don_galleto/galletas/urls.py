dee239654dd1d1a5fc05f31530f92b0d6892f4f2
LaAlejandraDev <alejandrasauceda2200@gmail.com>
2025-03-27 17:53:59 -0600

    Se agrego seguridad a las rutas de galletas, solictud de producción y producción

--- don_galleto/galletas/urls.py
+++ don_galleto/galletas/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from .views import AgregarGalletaRecetaView, ListaGalletasRecetaView, SeleccionarInsumosView, DetallesGalletaView, EditarGalletaRecetaView, EditarInsumosRecetaView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
-    path('agregar_galleta_receta/', AgregarGalletaRecetaView.as_view(), name='agregar_galleta_receta'),
-    path('lista_galletas_receta/', ListaGalletasRecetaView.as_view(), name='lista_galletas_receta'),
-    path('seleccionar_insumos_receta/<int:id_galleta>/', SeleccionarInsumosView.as_view(), name='seleccionar_insumos_receta'),
-    path('detalles_galleta_receta/<int:id_galleta>/', DetallesGalletaView.as_view(), name='detalles_galleta_receta'),
-    path('editar_galleta_receta/<int:id_galleta>/', EditarGalletaRecetaView.as_view(), name='editar_galleta_receta'),
-    path('editar_insumos_receta/<int:id_galleta>/', EditarInsumosRecetaView.as_view(), name='editar_insumos_receta'),
+    path('agregar_galleta_receta/', login_required(AgregarGalletaRecetaView.as_view()), name='agregar_galleta_receta'),
+    path('lista_galletas_receta/', login_required(ListaGalletasRecetaView.as_view()), name='lista_galletas_receta'),
+    path('seleccionar_insumos_receta/<int:id_galleta>/', login_required(SeleccionarInsumosView.as_view()), name='seleccionar_insumos_receta'),
+    path('detalles_galleta_receta/<int:id_galleta>/', login_required(DetallesGalletaView.as_view()), name='detalles_galleta_receta'),
+    path('editar_galleta_receta/<int:id_galleta>/', login_required(EditarGalletaRecetaView.as_view()), name='editar_galleta_receta'),
+    path('editar_insumos_receta/<int:id_galleta>/', login_required(EditarInsumosRecetaView.as_view()), name='editar_insumos_receta'),
 
 ]
