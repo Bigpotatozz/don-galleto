678e4e5ffe6bbf960e5fd794d4beaeee46d81c09
oscar <oscarmancilla227@gmail.com>
2025-04-01 10:22:37 -0600

    implementacion a2p iniciada

--- don_galleto/usuarios/urls.py
+++ don_galleto/usuarios/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from . import views
 from don_galleto.views import IndexView
 from django.contrib.auth.decorators import login_required
-from usuarios.views import  Lista_usuarios_view, Registro_admin_view, Edicion_usuario_view, eliminar_usuario
+from usuarios.views import  Lista_usuarios_view, Registro_admin_view, Edicion_usuario_view, eliminar_usuario, Login_view
 from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('listado_usuarios/', login_required(views.Lista_usuarios_view.as_view()), name='listado_usuarios'),
     path('registro_admin/', views.Registro_admin_view.as_view(), name='registro_admin'),
     path('editar_usuario/<int:id>/', views.Edicion_usuario_view.as_view(), name='editar_usuario'),
     path('eliminar_usuario/<int:id>/', eliminar_usuario, name='eliminar_usuario'),
+    path('my_login', views.Login_view.as_view(), name = 'mylogin' )
 ]
